faf13a4d89cbc8083ccb71c1777a19693f05162c
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2021-04-16 22:34:30 +0300

    Functional Programming - Exercise

---
+++ Advanced Module C#/04.Functional Programming/Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int divisible = int.Parse(Console.ReadLine());
+            Predicate<int> isDivisible = x => x % divisible == 0;
+            Func<List<int>, List<int>> reverse = result =>
+            { 
+                result.Reverse();
+                return result;
+            };
+            foreach (var number in reverse(numbers))
+            {
+                if (!isDivisible(number))
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
