4f923cad9b0267e356f08986fcef68ef93ac81a0
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2021-04-21 21:41:20 +0300

    DefiningClassesExercise

---
+++ Advanced Module C#/05.DefiningClasses/DefiningClassesExercise/09.PokemonTrainer/PokemonTrainer/Trainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Trainer
+{
+    public string Name { get; set; }
+    public int Badges { get; set; }
+    public List<Pokemon> Pokemons { get; set; }
+
+    public Trainer(string name)
+    {
+        this.Name = name;
+        this.Pokemons = new List<Pokemon>();
+    }
+}
