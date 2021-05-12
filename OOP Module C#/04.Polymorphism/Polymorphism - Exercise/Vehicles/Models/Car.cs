85fd859890ae23cb8fcd2823a326a646d53fae75
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2021-05-12 23:15:11 +0300

    Polymorphism - Exercise

---
+++ OOP Module C#/04.Polymorphism/Polymorphism - Exercise/Vehicles/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Vehicles.Common;
+
+namespace Vehicles.Models
+{
+
+    public class Car : Vehicle
+    {
+        private const double AIR_CONDITION = 0.9;
+        private const bool hole = false;
+        public Car(double fuelQuantity, double fuelConsumtpion)
+            : base(fuelQuantity, fuelConsumtpion)
+        {
+
+        }
+
+        protected override double AirCondition => AIR_CONDITION;
+
+        protected override bool Hole => hole;
+    }
+}
---
+++ OOP Module C#/04.Polymorphism/Polymorphism - Exercise/Vehicles/Models/Truck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Vehicles.Common;
+
+namespace Vehicles.Models
+{
+    public class Truck : Vehicle
+    {
+        private const double AIR_CONDITION = 1.6;
+        private const bool hole = true;
+
+        public Truck(double fuelQuantity, double fuelConsumtpion)
+            : base(fuelQuantity, fuelConsumtpion)
+        {
+        }
+
+        protected override double AirCondition => AIR_CONDITION;
+
+        protected override bool Hole => hole;
+    }
+}
