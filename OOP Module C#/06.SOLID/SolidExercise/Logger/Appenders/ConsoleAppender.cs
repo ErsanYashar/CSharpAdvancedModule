3942254ab483f9f3abea2adf0c10e729bcf183f5
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2021-05-15 18:22:19 +0300

    SOLID

---
+++ OOP Module C#/06.SOLID/SolidExercise/Logger/Appenders/ConsoleAppender.cs
@@ -0,0 +1,27 @@
+﻿using LogerExercise.Layout;
+using LoggerExercise.Enumerators;
+using System;
+
+namespace LoggerExercise.Appenders
+{
+    public class ConsoleAppender : Appender
+    {
+        public ConsoleAppender(ILayout layout) 
+            : base(layout)
+        {
+        }
+
+        public override void Append(string dateTime, ReportLevel report, string message)
+        {    
+            if(this.ReportLevel <= report)
+            {
+                this.MessagesAppended++;
+                Console.WriteLine(Layout.Format, dateTime, report, message);
+            }
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
