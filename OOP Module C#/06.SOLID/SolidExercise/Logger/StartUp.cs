1064e33474ee0c534f7509a4f5e61fa58b0528da
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2021-05-16 19:55:57 +0300

    SOLID

---
+++ OOP Module C#/06.SOLID/SolidExercise/Logger/StartUp.cs
@@ -0,0 +1,20 @@
+﻿
+using LogerExercise.Layout;
+using LoggerExercise.Appenders;
+using LoggerExercise.Engine;
+using LoggerExercise.Enumerators;
+using LoggerExercise.Layout;
+using LoggerExercise.Logger;
+using System;
+
+namespace LogerExercise
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {           
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
